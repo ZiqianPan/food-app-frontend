b668433627f676a6916f089191a8c5437fdcb142
ziqian pan <ziqianpan1998@gmail.com>
2023-07-31 00:37:00 -0400

    added search feature with some css update

--- src/components/FoodList/FoodList.js
+++ src/components/FoodList/FoodList.js
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import FoodCard from "../FoodCard/FoodCard";
 import "./FoodList.css";
 
 export default function FoodList({ foodData }) {
+  const [searchInput, setSearchInput] = useState("");
+
+  let dataToDisplay = foodData;
+
+  const handleChange = (e) => {
+    setSearchInput(e.target.value);
+  };
+
+  if (searchInput) {
+    dataToDisplay = foodData.filter((food) => {
+      const { name } = food;
+      return name.toLowerCase().includes(searchInput.toLowerCase());
+    });
+  }
+
+  const renderContent = () => {
+    if (dataToDisplay.length === 0) {
+      return <div className="FoodList__noResult">No results for:{searchInput}</div>;
+    } else {
+      return (
+        <div className="FoodList__content">
+          {dataToDisplay.map((food) => (
+            <FoodCard key={food.id} food={food} />
+          ))}
+        </div>
+      );
+    }
+  };
+
   return (
-    <div className="FoodList" >
-      {foodData.map((food) => (
-        <FoodCard key={food.id} food={food} />
-      ))}
+    <div className="FoodList">
+      <div className="FoodList__search">
+        <input
+          value={searchInput}
+          type="text"
+          placeholder="Search by name"
+          onChange={handleChange}
+        />
+      </div>
+      {renderContent()}
     </div>
   );
 }
