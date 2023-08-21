b71cc8890f508ae2268c9418f339c4d87f3d4015
ziqian pan <ziqianpan1998@gmail.com>
2023-08-21 17:57:38 -0400

    updated mobile and tablet responsive

--- src/components/FoodCard/FoodCard.js
+++ src/components/FoodCard/FoodCard.js
@@ -3,17 +3,20 @@ import "./FoodCard.css";
 
 export default function FoodCard({ food }) {
   const { id, name, price, image, toppings, shortDescription } = food;
+  
   return (
     <div className="FoodCard" key={id}>
       <h1>{name}</h1>
       <span className="FoodCar__shortDescriptions">{shortDescription}</span>
       <img src={image} alt={name} />
       <div className="FoodCard__price">
-        <span><strong>Price</strong>: ${price.toFixed(2)}</span>
+        <span>
+          <strong>Price</strong>: ${price.toFixed(2)}
+        </span>
       </div>
       <div className="FoodCard__toppings">
         <span>
-        <strong>Toppings</strong>:{" "}
+          <strong>Toppings</strong>:{" "}
           {toppings.length !== 0
             ? toppings.map((topping, index) => (
                 <span key={index}>
